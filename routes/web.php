31e586544a68eeffa7f4a7e5af2e70e369e110a5
Rizky riaadha rismandana <riaadha.rizky@gmail.com>
2025-08-25 17:38:33 +0700

    feat: add lecture & utbk session controllers with routes

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    Route::resource('lectures', LectureController::class);
+Route::resource('utbk-sessions', UtbkSessionController::class);
+
 });
